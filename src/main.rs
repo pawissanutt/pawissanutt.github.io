1fdc60e53bb5b93f50ac2063cb494f1def5d634a
Pawissanutt Lertpongrujikorn <pawissanutt@gmail.com>
2025-11-10 15:58:57 -0600

    fix: Correct asset paths in main.rs and ensure base_path is set in Dioxus.toml

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,9 @@ pub enum Route {
     Home {},
 }
 
-const FAVICON: Asset = asset!("/assets/favicon.ico");
-const MAIN_CSS: Asset = asset!("/assets/main.css");
-const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
+const FAVICON: Asset = asset!("assets/favicon.ico");
+const MAIN_CSS: Asset = asset!("assets/main.css");
+const TAILWIND_CSS: Asset = asset!("assets/tailwind.css");
 
 fn main() {
     dioxus::launch(App);
